75ef33eb7269bbc7867fff0a585aac9f74174e8d
quqixun <quqixun@gmail.com>
2022-11-27 21:41:33 +0800

    test preprocessing

--- misc/statistics.py
+++ misc/statistics.py
@@ -10,7 +10,31 @@ from tqdm import tqdm
 from os.path import join as opj
 
 
-# matplotlib.use('Agg')
+matplotlib.use('Agg')
+
+
+PROCESS_INFO = {
+    'label': {'2pow': -6, 'min': -3.0, 'max': 14.0},
+    'S1': {
+        0: {'2pow': None, 'min': -25.0, 'max': 30.0},
+        1: {'2pow': None, 'min': -61.0, 'max': 30.0},
+        2: {'2pow': None, 'min': -25.0, 'max': 30.0},
+        3: {'2pow': None, 'min': -70.0, 'max': 20.0},
+    },
+    'S2': {
+        0:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        1:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        2:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        3:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        4:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        5:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        6:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        7:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        8:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        9:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        10: {'2pow': None, 'min': 0.0, 'max': 105.0},
+    }
+}
 
 
 def read_raster(data_path):
@@ -24,7 +48,39 @@ def read_raster(data_path):
     return data
 
 
-def calc_stats(data, data_name, p=None, hist=True):
+def process_data(data, data_name, data_index=None):
+
+    if data_name == 'label':
+        process_dict = PROCESS_INFO['label']
+    else:
+        process_dict = PROCESS_INFO[data_name][data_index]
+    
+    if process_dict['2pow'] is not None:
+        min_thresh = 2 ** process_dict['2pow']
+        data = np.where(data < min_thresh, min_thresh, data)
+        data = np.log2(data)
+    
+    min_ = process_dict['min']
+    max_ = process_dict['max']
+    range_ = max_ - min_
+
+    if (data_name == 'S2') and (data_index == 10):
+        data = np.where(data < min_, min_, data)
+        data = np.where(data > max_, min_, data)
+    else:
+        data = np.where(data < min_, min_, data)
+        data = np.where(data > max_, max_, data)
+    data = (data - min_) / range_
+
+    return data
+
+
+def calc_stats(data, data_name, exclude_zero=False, p=None, hist=True):
+
+    data_ = data.copy()
+
+    if exclude_zero:
+        data = data_[np.where(data > 0)]
 
     if p is not None:
         assert 0 <= p <= 100
@@ -46,14 +102,15 @@ def calc_stats(data, data_name, p=None, hist=True):
     print(f'- std: {data_std:.3f}')
 
     if hist:
-        plot_dir = './data/source/plot'
+        plot_dir = f'./data/source/plot/'
         os.makedirs(plot_dir, exist_ok=True)
         plot_file = f'stats_{data_name}_p{p}.png'
         plot_path = opj(plot_dir, plot_file)
 
         plt.figure()
         plt.title(f'{data_name} - P:{p}')
-        plt.hist(data.reshape(-1), bins=100, log=True)
+        plt.hist(data.reshape(-1), bins=100, log=False)
+        plt.tight_layout()
         # plt.show()
         plt.savefig(plot_path)
         plt.close()
@@ -78,7 +135,6 @@ if __name__ == '__main__':
     # statistics of label
 
     label_list = []
-    stats['label'] = {}
     for subject in tqdm(subjects, ncols=88):
         subject_dir = opj(data_dir, subject)
 
@@ -89,15 +145,9 @@ if __name__ == '__main__':
             label_list.append(label)
 
     label = np.array(label_list)
-    stats['label'][None] = calc_stats(label, 'label', p=None)
-    stats['label'][99.5] = calc_stats(label, 'label', p=99.5)
-    stats['label'][99.9] = calc_stats(label, 'label', p=99.9)
-
-    label = np.where(label < 2 ** (-6), 2 ** (-6), label)
-    label_log2 = np.log2(label)
-    stats['label']['log2'] = calc_stats(label_log2, 'label-log2', p=None)
-
-    del label_list, label, label_log2
+    label = process_data(label, 'label')
+    stats['label'] = calc_stats(label, 'label', exclude_zero=False, p=None)
+    del label_list, label
     gc.collect()
 
     # --------------------------------------------------------------------------
@@ -107,27 +157,26 @@ if __name__ == '__main__':
     for fname, fnum in feat_dict.items():
         stats[fname] = {}
 
-        for i in range(fnum):
+        for index in range(fnum):
             ith_feat_list = []
-            stats[fname][i] = {}
 
-            for subject in tqdm(subjects, ncols=88):
+            print(f'Feature: {fname} - index: {index}')
+            for subject in tqdm(subjects[:1000], ncols=88):
                 subject_dir = opj(data_dir, subject)
 
-                for j in range(12):
-                    feat_file = f'{subject}_{fname}_{j:02d}.tif'
+                for month in range(12):
+                    feat_file = f'{subject}_{fname}_{month:02d}.tif'
                     feat_path = opj(subject_dir, fname, feat_file)
                     feat = read_raster(feat_path)
                     if feat is not None:
                         assert feat.shape[0] == fnum
-                        ith_feat = feat[i]
+                        ith_feat = feat[index]
+                        ith_feat = process_data(ith_feat, fname, index)
                         ith_feat_list.append(ith_feat)
 
             ith_feat = np.array(ith_feat_list)
-            ith_fname = f'{fname}-{i}'
-            stats[fname][i][None] = calc_stats(ith_feat, ith_fname, p=None)
-            stats[fname][i][99.5] = calc_stats(ith_feat, ith_fname, p=99.5)
-            stats[fname][i][99.9] = calc_stats(ith_feat, ith_fname, p=99.9)
+            ith_fname = f'{fname}-{index}'
+            stats[fname][index] = calc_stats(ith_feat, ith_fname, exclude_zero=True, p=None)
             del ith_feat_list, ith_feat
             gc.collect()
 
@@ -137,3 +186,6 @@ if __name__ == '__main__':
     stats_path = './data/source/stats.pkl'
     with open(stats_path, 'wb') as f:
         pickle.dump(stats, f)
+
+    print(stats)
+    
--- misc/visualize.py
+++ misc/visualize.py
@@ -11,6 +11,28 @@ GT_SHAPE = (1,  256, 256)
 S1_SHAPE = (4,  256, 256)
 S2_SHAPE = (11, 256, 256)
 
+PROCESS_INFO = {
+    'label': {'2pow': -6, 'min': -3.0, 'max': 14.0},
+    'S1': {
+        0: {'2pow': None, 'min': -25.0, 'max': 30.0},
+        1: {'2pow': None, 'min': -61.0, 'max': 30.0},
+        2: {'2pow': None, 'min': -25.0, 'max': 30.0},
+        3: {'2pow': None, 'min': -70.0, 'max': 20.0},
+    },
+    'S2': {
+        0:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        1:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        2:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        3:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        4:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        5:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        6:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        7:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        8:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        9:  {'2pow': -1,   'min': 0.0, 'max': 15.0 },
+        10: {'2pow': None, 'min': 0.0, 'max': 110.0},
+    }
+}
 
 
 def read_raster(data_path, data_shape=None):
@@ -25,11 +47,40 @@ def read_raster(data_path, data_shape=None):
     return data
 
 
+def process_data(data, data_name, data_index=None):
+
+    if data_name == 'label':
+        process_dict = PROCESS_INFO['label']
+    else:
+        process_dict = PROCESS_INFO[data_name][data_index]
+    
+    if process_dict['2pow'] is not None:
+        min_thresh = 2 ** process_dict['2pow']
+        data = np.where(data < min_thresh, min_thresh, data)
+        data = np.log2(data)
+    
+    min_ = process_dict['min']
+    max_ = process_dict['max']
+    range_ = max_ - min_
+
+    if (data_name == 'S2') and (data_index == 10):
+        data = np.where(data < min_, min_, data)
+        data = np.where(data > max_, min_, data)
+    else:
+        data = np.where(data < min_, min_, data)
+        data = np.where(data > max_, max_, data)
+    data = (data - min_) / range_
+
+    return data
+
+
 if __name__ == '__main__':
 
     data_dir = './data/source/train'
     subjects = os.listdir(data_dir)
     subjects.sort()
+
+    plot_kwargs = {'vmin': 0.0, 'vmax': 1.0, 'cmap': 'coolwarm'}
 
     for subject in tqdm(subjects, ncols=88):
         subject_dir = opj(data_dir, subject)
@@ -37,6 +88,7 @@ if __name__ == '__main__':
         # load label data
         label_path = opj(subject_dir, f'{subject}_agbm.tif')
         label = read_raster(label_path, GT_SHAPE)
+        label = process_data(label, 'label')
 
         # load features
         for i in range(12):
@@ -48,17 +100,23 @@ if __name__ == '__main__':
             plt.figure(f'{subject} - {i:02d}', figsize=(15, 15))
             plt.subplot(4, 4, 1)
             plt.title('GT')
-            plt.imshow(label[0])
+            plt.imshow(label[0], **plot_kwargs)
             plt.axis('off')
-            for is1 in range(s1.shape[0]):
-                plt.subplot(4, 4, is1 + 2)
-                plt.title(f'S1-{is1 + 1}')
-                plt.imshow(s1[is1])
+            for s1_index in range(s1.shape[0]):
+                s1_index_data = s1[s1_index]
+                s1_index_data = process_data(s1_index_data, 'S1', s1_index)
+                plt.subplot(4, 4, s1_index + 2)
+                plt.title(f'S1-{s1_index + 1}')
+                # plt.imshow(s1[s1_index])
+                plt.imshow(s1_index_data, **plot_kwargs)
                 plt.axis('off')
-            for is2 in range(s2.shape[0]):
-                plt.subplot(4, 4, is2 + 2 + s1.shape[0])
-                plt.title(f'S2-{is2 + 1}')
-                plt.imshow(s2[is2])
+            for s2_index in range(s2.shape[0]):
+                s2_index_data = s2[s2_index]
+                s2_index_data = process_data(s2_index_data, 'S2', s2_index)
+                plt.subplot(4, 4, s2_index + 2 + s1.shape[0])
+                plt.title(f'S2-{s2_index + 1}')
+                # plt.imshow(s2[s2_index])
+                plt.imshow(s2_index_data, **plot_kwargs)
                 plt.axis('off')
             plt.tight_layout()
             plt.show()
