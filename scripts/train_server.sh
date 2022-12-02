3e1b51ae6a077cc50d7099a1b6bf9d643af790c0
quqixun <quqixun@gmail.com>
2022-12-02 19:19:19 +0800

    test training

--- scripts/train_server.sh
+++ scripts/train_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-CUDA_VISIBLE_DEVICES=0                \
+CUDA_VISIBLE_DEVICES=2,3              \
 python train.py                       \
-    --data_root   ./data/source       \
+    --data_root   /mnt/dataset/quqixun/Github/BioMassters/data/source \
     --exp_root    ./experiments       \
     --config_file ./configs/exp1.yaml \
     --folds       0,1,2,3,4
