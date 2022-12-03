1c86bd7d3f40d6e32152290014a0aaafb3db516f
quqixun <quqixun@gmail.com>
2022-12-03 23:19:49 +0800

    training swin_unetr

--- scripts/train_gpu2.sh
+++ scripts/train_gpu2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-CUDA_VISIBLE_DEVICES=2,3              \
+CUDA_VISIBLE_DEVICES=2                \
 python train.py                       \
     --data_root   /mnt/dataset/quqixun/Github/BioMassters/data/source \
     --exp_root    ./experiments       \
     --config_file ./configs/exp1.yaml \
-    --folds       0,1,2,3,4
+    --folds       0,1
---
+++ scripts/train_gpu3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+CUDA_VISIBLE_DEVICES=3                \
+python train.py                       \
+    --data_root   /mnt/dataset/quqixun/Github/BioMassters/data/source \
+    --exp_root    ./experiments       \
+    --config_file ./configs/exp1.yaml \
+    --folds       2,3
