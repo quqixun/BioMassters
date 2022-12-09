1464b8613e1ccd88c41209971938a92fd0f0e869
quqixun <quqixun@gmail.com>
2022-12-09 19:23:49 +0800

    more exps

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -2,11 +2,24 @@
 
 
 device=0
+data_root=./data/source
 
 
 CUDA_VISIBLE_DEVICES=$device \
 python predict.py            \
-    --data_root   ./data/source       \
-    --exp_root    ./experiments       \
-    --output_root ./predictions       \
-    --config_file ./configs/exp2.yaml
+    --data_root      $data_root          \
+    --exp_root       ./experiments/log2  \
+    --output_root    ./predictions/log2  \
+    --config_file    ./configs/exp1.yaml \
+    --process_method log2                \
+    --folds          0,3
+
+
+# CUDA_VISIBLE_DEVICES=$device \
+# python predict.py            \
+#     --data_root      $data_root          \
+#     --exp_root       ./experiments/plain \
+#     --output_root    ./predictions/plain \
+#     --config_file    ./configs/exp1.yaml \
+#     --process_method plain               \
+#     --folds          0,1,2,3,4
--- scripts/train.sh
+++ scripts/train.sh
@@ -7,20 +7,20 @@ config_file=./configs/exp1.yaml
 
 CUDA_VISIBLE_DEVICES=$device \
 python train.py              \
-    --data_root      ./data/source       \
-    --exp_root       ./experiments/log2  \
-    --config_file    $config_file        \
-    --folds          0,1,2               \
+    --data_root      ./data/source      \
+    --exp_root       ./experiments/log2 \
+    --config_file    $config_file       \
+    --folds          0,1,23,4           \
     --process_method log2
 
 
-CUDA_VISIBLE_DEVICES=$device \
-python train.py              \
-    --data_root      ./data/source       \
-    --exp_root       ./experiments/plain \
-    --config_file    $config_file        \
-    --folds          3,4                 \
-    --process_method plain
+# CUDA_VISIBLE_DEVICES=$device \
+# python train.py              \
+#     --data_root      ./data/source       \
+#     --exp_root       ./experiments/plain \
+#     --config_file    $config_file        \
+#     --folds          0,1,2,3,4           \
+#     --process_method plain
 
 
 # CUDA_VISIBLE_DEVICES=$device \
