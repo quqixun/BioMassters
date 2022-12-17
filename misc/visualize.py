67c1503f204e5bdd91fcec9c309eab40ac477960
quqixun <quqixun@gmail.com>
2022-12-17 19:59:42 +0800

    modifu visualization

--- misc/visualize.py
+++ misc/visualize.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,69 +8,60 @@ from tqdm import tqdm
 from os.path import join as opj
 
 
+warnings.filterwarnings('ignore')
+
+
 GT_SHAPE = (1,  256, 256)
 S1_SHAPE = (4,  256, 256)
 S2_SHAPE = (11, 256, 256)
 
-PROCESS_INFO = {
-    'label': {'2pow': -6, 'min': -3.0, 'max': 14.0},
+INLIERS_PLAIN = {
     'S1': {
-        0: {'2pow': None, 'min': -25.0, 'max': 30.0},
-        1: {'2pow': None, 'min': -63.0, 'max': 29.0},
-        2: {'2pow': None, 'min': -25.0, 'max': 32.0},
-        3: {'2pow': None, 'min': -70.0, 'max': 23.0},
+        0: {'min': -25.0, 'max': 30.0},
+        1: {'min': -63.0, 'max': 29.0},
+        2: {'min': -25.0, 'max': 32.0},
+        3: {'min': -70.0, 'max': 23.0},
     },
     'S2': {
-        0:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        1:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        2:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        3:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        4:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        5:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        6:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        7:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        8:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        9:  {'2pow': -1,   'min': 0.0, 'max': 14.0 },
-        10: {'2pow': None, 'min': 0.0, 'max': 100.0},
+        10: {'min': 0.0, 'max': 100.0},
     }
 }
 
 
-def read_raster(data_path, data_shape=None):
+def remove_outliers_by_plain(data, data_name, data_index=None):
+
+    if data_name == 'label':
+        data = np.where(data < 0.0, 0.0, data)
+
+    elif data_name == 'S1':
+        inlier_dict = INLIERS_PLAIN['S1'][data_index]
+        min_ = inlier_dict['min']
+        max_ = inlier_dict['max']
+        data = np.where(data < min_, min_, data)
+        data = np.where(data > max_, max_, data)
+
+    elif data_name == 'S2':
+        if data_index == 10:
+            inlier_dict = INLIERS_PLAIN['S2'][10]
+            min_ = inlier_dict['min']
+            max_ = inlier_dict['max']
+            data = np.where(data < min_, min_, data)
+            data = np.where(data > max_, min_, data)
+
+    return data
+
+
+def read_raster(data_path, return_zeros=False, data_shape=None):
 
     if os.path.isfile(data_path):
         raster = rasterio.open(data_path)
         data = raster.read()
     else:
-        assert data_shape is not None
-        data = np.zeros(data_shape).astype(np.float32)
-
-    return data
-
-
-def process_data(data, data_name, data_index=None):
-
-    if data_name == 'label':
-        process_dict = PROCESS_INFO['label']
-    else:
-        process_dict = PROCESS_INFO[data_name][data_index]
-    
-    if process_dict['2pow'] is not None:
-        min_thresh = 2 ** process_dict['2pow']
-        data = np.where(data < min_thresh, min_thresh, data)
-        data = np.log2(data)
-    
-    min_ = process_dict['min']
-    max_ = process_dict['max']
-    range_ = max_ - min_
-
-    if (data_name == 'S2') and (data_index == 10):
-        data = np.where(data < min_, min_, data)
-        data = np.where(data > max_, min_, data)
-    else:
-        data = np.where(data < min_, min_, data)
-        data = np.where(data > max_, max_, data)
-    data = (data - min_) / range_
+        if return_zeros:
+            assert data_shape is not None
+            data = np.zeros(data_shape).astype(np.float32)
+        else:
+            data = None
 
     return data
 
@@ -80,41 +72,39 @@ if __name__ == '__main__':
     subjects = os.listdir(data_dir)
     subjects.sort()
 
-    plot_kwargs = {'vmin': 0.0, 'vmax': 1.0, 'cmap': 'coolwarm'}
-
     for subject in tqdm(subjects, ncols=88):
         subject_dir = opj(data_dir, subject)
 
         # load label data
         label_path = opj(subject_dir, f'{subject}_agbm.tif')
-        label = read_raster(label_path, GT_SHAPE)
-        label = process_data(label, 'label')
+        label = read_raster(label_path, return_zeros=True, data_shape=GT_SHAPE)
+        label = remove_outliers_by_plain(label, 'label')
 
         # load features
         for i in range(12):
             s1_path = opj(subject_dir, 'S1', f'{subject}_S1_{i:02d}.tif')
             s2_path = opj(subject_dir, 'S2', f'{subject}_S2_{i:02d}.tif')
-            s1 = read_raster(s1_path, S1_SHAPE)
-            s2 = read_raster(s2_path, S2_SHAPE)
+            s1 = read_raster(s1_path, return_zeros=True, data_shape=S1_SHAPE)
+            s2 = read_raster(s2_path, return_zeros=True, data_shape=S2_SHAPE)
 
             plt.figure(f'{subject} - {i:02d}', figsize=(15, 15))
             plt.subplot(4, 4, 1)
             plt.title('GT')
-            plt.imshow(label[0], **plot_kwargs)
+            plt.imshow(label[0])
             plt.axis('off')
             for s1_index in range(s1.shape[0]):
                 s1_index_data = s1[s1_index]
-                s1_index_data = process_data(s1_index_data, 'S1', s1_index)
+                s1_index_data = remove_outliers_by_plain(s1_index_data, 'S1', s1_index)
                 plt.subplot(4, 4, s1_index + 2)
                 plt.title(f'S1-{s1_index + 1}')
-                plt.imshow(s1_index_data, **plot_kwargs)
+                plt.imshow(s1_index_data)
                 plt.axis('off')
             for s2_index in range(s2.shape[0]):
                 s2_index_data = s2[s2_index]
-                s2_index_data = process_data(s2_index_data, 'S2', s2_index)
+                s2_index_data = remove_outliers_by_plain(s2_index_data, 'S2', s2_index)
                 plt.subplot(4, 4, s2_index + 2 + s1.shape[0])
                 plt.title(f'S2-{s2_index + 1}')
-                plt.imshow(s2_index_data, **plot_kwargs)
+                plt.imshow(s2_index_data)
                 plt.axis('off')
             plt.tight_layout()
             plt.show()
