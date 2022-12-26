836a5211855cb69729bade54aade467abf732d00
quqixun <quqixun@gmail.com>
2022-12-26 11:56:10 +0800

    new exps

--- scripts/train_gpu2.sh
+++ scripts/train_gpu2.sh
@@ -6,15 +6,9 @@ folds=0
 # folds=1,2
 process=plain
 config_file_list=(
-    # ./configs/swin_unetr/exp2.yaml
-    # ./configs/swin_unetr/exp3.yaml
-    # ./configs/swin_unetr/exp4.yaml
-    # ./configs/swin_unetr/exp4-2.yaml
-    # ./configs/swin_unetr/exp6.yaml
-    # ./configs/swin_unetr/exp7.yaml
-    # ./configs/swin_unetr/exp10.yaml
     ./configs/swin_unetr/exp11.yaml
-    # ./configs/swin_unetr/exp12.yaml
+    # ./configs/swin_unetr/exp15.yaml
+    # ./configs/swin_unetr/exp16.yaml
 )
 
 for config_file in ${config_file_list[@]}; do
--- scripts/train_gpu3.sh
+++ scripts/train_gpu3.sh
@@ -6,13 +6,9 @@ folds=0
 # folds=3,4
 process=plain
 config_file_list=(
-    # ./configs/swin_unetr/exp4.yaml
-    # ./configs/swin_unetr/exp5.yaml
-    # ./configs/swin_unetr/exp8.yaml
-    # ./configs/swin_unetr/exp9.yaml
-    # ./configs/swin_unetr/exp10.yaml
     ./configs/swin_unetr/exp12.yaml
-    # ./configs/vt2unet/exp1.yaml
+    # ./configs/swin_unetr/exp17.yaml
+    # ./configs/swin_unetr/exp18.yaml
 )
 
 for config_file in ${config_file_list[@]}; do
