15fac8f87f5a015c32940cf0063960e925af914e
quqixun <quqixun@gmail.com>
2023-01-27 21:36:29 +0800

    keep experiment for submission

--- scripts/process.sh
+++ scripts/process.sh
@@ -5,9 +5,9 @@ source_root=./data/source
 split_seed=42
 split_folds=5
 
-python process.py \
-    --source_root    $source_root \
-    --process_method log2
+# python process.py \
+#     --source_root    $source_root \
+#     --process_method log2
 
 python process.py \
     --source_root    $source_root \
